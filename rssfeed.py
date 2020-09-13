0a2ffa241bc270cfcd408c279d261806f0d34009
Alex Bandisch <alexander.bandisch@gmail.com>
2020-09-13 10:04:58 +1000

    use published date

--- rssfeed.py
+++ rssfeed.py
@@ -3,6 +3,7 @@ from io import StringIO
 from html.parser import HTMLParser
 import re
 import requests
+import datetime
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -19,7 +20,8 @@ class MLStripper(HTMLParser):
 def main():
   try:
     # feed = "http://podcastfeeds.nbcnews.com/drone/api/query/audio/podcast/1.0/MSNBC-MADDOW-NETCAST-MP3.xml"
-    feed = input("Enter the feed xml url: ")
+    feed = "http://joeroganexp.joerogan.libsynpro.com/rss"
+    # feed = input("Enter the feed xml url: ")
     NewsFeed = feedparser.parse(feed)
 
     entry = NewsFeed.entries[1]
@@ -27,16 +29,18 @@ def main():
     # entry.summary - entry.title .mp3
     # "August 21 2020 - Bannon legal woes leave Trump scrambling for distance (again).mp3"
 
-    for x in NewsFeed.entries:
-      s = MLStripper()
-      s.feed(x.summary)
-      titleDate = re.sub('[^0-9a-zA-Z]+', '-', s.get_data())
-      # print('link: %s' % x.links[0].href)
-      # print('date: %s' % titleDate)
-      # print('title: %s' % x.title)
-      # print('value: %s' % x.summary_detail.value)
-      filename = titleDate + " - " + x.title + ".mp3"
+    # # print('NewsFeed.entries[1]: %s ' % NewsFeed.entries[1])
+    # print('NewsFeed.entries[1].title: %s ' % NewsFeed.entries[1].title)
+    # print('NewsFeed.entries[1].published: %s ' % NewsFeed.entries[1].published)
+    # titleDateObj = datetime.datetime.strptime(NewsFeed.entries[1].published, '%a, %d %b %Y %H:%M:%S %z')
+    # print('titleDate: %s ' % titleDateObj.date())
 
+    for x in NewsFeed.entries:
+      # s = MLStripper()
+      # s.feed(x.summary)
+      # titleDate = re.sub('[^0-9a-zA-Z]+', '-', s.get_data())
+      titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
+      filename = str(titleDateObj.date()) + " - " + x.title + ".mp3"
       print('downloading filename: %s' % filename)
       r = requests.get(x.links[0].href, allow_redirects=True)
       open(filename, 'wb').write(r.content)
