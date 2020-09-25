d9ce5d87480b1c4e00562b3bda1e53ca60774d0b
Alex Bandisch <alexander.bandisch@gmail.com>
2020-09-26 09:44:14 +1000

    store mp3s in podcast subdir

--- rssfeed.py
+++ rssfeed.py
@@ -29,12 +29,28 @@ def main():
       print('  the "%s" folder doesnt exist. Creating it now ... ' % downloadsPath)
       makedirs(downloadsPath)
 
-    # feed = "http://podcastfeeds.nbcnews.com/drone/api/query/audio/podcast/1.0/MSNBC-MADDOW-NETCAST-MP3.xml"
-    feed = "http://joeroganexp.joerogan.libsynpro.com/rss"
+    # Get the feed URL
+    feed = "http://podcastfeeds.nbcnews.com/drone/api/query/audio/podcast/1.0/MSNBC-MADDOW-NETCAST-MP3.xml"
+    # feed = "http://joeroganexp.joerogan.libsynpro.com/rss"
     # feed = input("Enter the feed xml url: ")
+
+    # Parse the feed URL
     NewsFeed = feedparser.parse(feed)
 
-    entry = NewsFeed.entries[1]
+    # Get the Channel Title
+    channelTitle = NewsFeed['channel']['title']
+    channelTitlePath = downloadsPath + "/" + channelTitle
+
+    # Check to see if Channel Title directory exists under downloads directory
+    channelTitleDirExists = path.exists(channelTitlePath) and path.isdir(channelTitlePath)
+
+    # if not, create Channel Title directory under downloads directory
+    if not channelTitleDirExists:
+      print('  the channel folder "%s" doesnt exist. Creating it now ... ' % channelTitlePath)
+      makedirs(channelTitlePath)
+
+
+    # entry = NewsFeed.entries[1]
 
     # entry.summary - entry.title .mp3
     # "August 21 2020 - Bannon legal woes leave Trump scrambling for distance (again).mp3"
@@ -50,7 +66,7 @@ def main():
       # s.feed(x.summary)
       # titleDate = re.sub('[^0-9a-zA-Z]+', '-', s.get_data())
       titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
-      filename = str(titleDateObj.date()) + " - " + x.title + ".mp3"
+      filename = channelTitlePath + "/" + str(titleDateObj.date()) + " - " + x.title + ".mp3"
       print('downloading filename: %s' % filename)
       r = requests.get(x.links[0].href, allow_redirects=True)
       open(filename, 'wb').write(r.content)
