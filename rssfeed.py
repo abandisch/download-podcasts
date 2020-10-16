6b2dcade047380433780f70df254e7d0e24fca27
Alex Bandisch <alexander.bandisch@gmail.com>
2020-10-17 09:59:16 +1000

    add new features

--- rssfeed.py
+++ rssfeed.py
@@ -5,18 +5,7 @@ from os import path, makedirs
 import re
 import requests
 import datetime
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.text = StringIO()
-    def handle_data(self, d):
-        self.text.write(d)
-    def get_data(self):
-        return self.text.getvalue()
+from urllib.parse import urlparse
 
 def main():
   try:
@@ -29,45 +18,53 @@ def main():
       print('  the "%s" folder doesnt exist. Creating it now ... ' % downloadsPath)
       makedirs(downloadsPath)
 
-    # Get the feed URL
-    # feed = "http://podcastfeeds.nbcnews.com/drone/api/query/audio/podcast/1.0/MSNBC-MADDOW-NETCAST-MP3.xml"
-    # feed = "http://joeroganexp.joerogan.libsynpro.com/rss"
-    feed = input("Enter the feed xml url: ")
+    # Sample feed URLs
+    # http://podcastfeeds.nbcnews.com/drone/api/query/audio/podcast/1.0/MSNBC-MADDOW-NETCAST-MP3.xml
+    # http://joeroganexp.joerogan.libsynpro.com/rss
 
+    # Get the feed URL
+    feed = input("Enter the feed xml url (e.g. http://podcastdomain.com/path/to/file.xml): ")
+
+    # Check if feed is vaild URL
+    parsedUrl = urlparse(feed)
+
+    # Check to see if parsedUrl.scheme, parsedUrl.netloc and parsedUrl.path have values
+    if parsedUrl.scheme == "" or parsedUrl.netloc == "" or parsedUrl.path == "":
+      # if not, throw an exception
+      raise Exception("Invalid URL: %s" % feed)
+
+    print('  parsing URL ...', end = '')
     # Parse the feed URL
     NewsFeed = feedparser.parse(feed)
+    print('done')
+
+    # Check to see Channel and Title exist
+    print('  Confirming valid title ...', end = '')
+    if type(NewsFeed) is not dict or not NewsFeed.has_key('channel'):
+      # if not, throw an exception
+      raise Exception("Invalid channel or title")
+    print('done')
 
     # Get the Channel Title
     channelTitle = NewsFeed['channel']['title']
     channelTitlePath = downloadsPath + "/" + channelTitle
 
+    print('  checking if podcast directory exists ...')
     # Check to see if Channel Title directory exists under downloads directory
     channelTitleDirExists = path.exists(channelTitlePath) and path.isdir(channelTitlePath)
 
     # if not, create Channel Title directory under downloads directory
     if not channelTitleDirExists:
-      print('  the channel folder "%s" doesnt exist. Creating it now ... ' % channelTitlePath)
+      print('  the channel folder "%s" doesnt exist. Creating it now ... ' % channelTitlePath, end = '')
       makedirs(channelTitlePath)
 
+    print('done')
 
-    # entry = NewsFeed.entries[1]
-
-    # entry.summary - entry.title .mp3
-    # "August 21 2020 - Bannon legal woes leave Trump scrambling for distance (again).mp3"
-
-    # # print('NewsFeed.entries[1]: %s ' % NewsFeed.entries[1])
-    # print('NewsFeed.entries[1].title: %s ' % NewsFeed.entries[1].title)
-    # print('NewsFeed.entries[1].published: %s ' % NewsFeed.entries[1].published)
-    # titleDateObj = datetime.datetime.strptime(NewsFeed.entries[1].published, '%a, %d %b %Y %H:%M:%S %z')
-    # print('titleDate: %s ' % titleDateObj.date())
-
+    # Download the MP3 files
     for x in NewsFeed.entries:
-      # s = MLStripper()
-      # s.feed(x.summary)
-      # titleDate = re.sub('[^0-9a-zA-Z]+', '-', s.get_data())
       titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
       filename = channelTitlePath + "/" + str(titleDateObj.date()) + " - " + x.title + ".mp3"
-      print('downloading filename: %s' % filename)
+      print('  downloading filename: %s ... ' % filename, end = '')
       r = requests.get(x.links[0].href, allow_redirects=True)
       open(filename, 'wb').write(r.content)
       print('done')
