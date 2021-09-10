cc04d020f0f3daede9f3b4dadb6a8f58f9021ef6
Delia Codes <delia@delia.codes>
2021-09-10 17:38:05 -0400

    Handles downloading large podcast feeds.

--- src/rssfeed.py
+++ src/rssfeed.py
@@ -1,4 +1,5 @@
 import feedparser
+import shutil
 import podcastparser
 from io import StringIO
 from html.parser import HTMLParser
@@ -10,6 +11,23 @@ import datetime
 from urllib.parse import urlparse
 import pprint
 # from json import load, loads
+
+def check_if_file_link(poss_link):
+  if poss_link:
+    # check if poss_link has mp3, other audio ends at the end
+    poss_link
+
+def get_ending(linkname):
+  if linkname:
+    split_link = linkname.split(".")
+    # get last el of the string
+
+def download_file(url, filename):
+  with requests.get(url, stream=True) as r:
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+  return True
+
 
 def main():
   try:
@@ -81,27 +99,28 @@ def main():
       pp = pprint.PrettyPrinter(indent=4)
       if parsedFromParser:
         for ep in parsedFromParser["episodes"]:
-          # print("~~~~~~~~~~~~LINK~~~~~~~~~~",pp.pprint(ep))
-          possible_enclosures = ep["enclosures"]
-          # TODO create a method that will check to see if something is a url ending in an "mp3" or similar link.
-
-          if possible_enclosures:
-            list_of_ep = ep["enclosures"]
-            link = list_of_ep[0]["url"]
-            r = requests.get(link, allow_redirects=True)
-          
+          print("~~~~~~~~~~~~~~~~~~~~~~~~~~~EP~~~~~~~~~~~~~~~~~~~~~~",pp.pprint(ep.get("enclosures", 'No Enclosures Found')))
+          possible_enclosures = ep.get("enclosures", None)
           # TODO we should get the extension of the file of the url and use it rather than manually setting mp3
+          
           if ep["title"]:
             filename = ep["title"] + ".mp3"
           elif parsedFromParser["description"]:
             filename = parsedFromParser["description"] + '-{ep_count}' ".mp3"
           else:
-            filename = '{ep_count}.mp3'
+            filename = " %d " % (ep_count)
+
+          if possible_enclosures:
+            list_of_ep = ep["enclosures"]
+            link = list_of_ep[0]["url"]
+            download_file(link, channelTitlePath + "/" + filename)
+          
 
           # TODO For next time, sanitize file names
-          # TODO get podcast actually working - ep link should end in mp3
+          # TODO create a method that will check to see if something is a url ending in an "mp3" or similar link.
+          # TODO handle erroring out when link doesn't end in an audio file extension
 
-          open(channelTitlePath + "/" + filename, 'wb').write(r.content)
+
           # titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
           # filename = channelTitlePath + "/" + str(titleDateObj.date()) + " - " + x.title + ".mp3"
           # print('  downloading filename: %s ... ' % filename, end = '')
