6255dab80cb98f40de233edc092bd1dd2a5bf0e8
Delia Codes <delia@delia.codes>
2021-10-08 19:53:04 -0400

    Removes feed from the front of a url. Also cleans up commented out code and adds a few more todos.

--- src/rssfeed.py
+++ src/rssfeed.py
@@ -12,6 +12,22 @@ from urllib.parse import urlparse
 import pprint
 # from json import load, loads
 
+def remove_feed_from_url(url):
+  if url:
+    conv_url = str(url)
+    # TODO this could easily be cleaner with regex
+
+    check_for_feed = conv_url[0:4].lower()
+    print("FEED", check_for_feed)
+    if check_for_feed == 'feed':
+      new_url = url[5:]
+      print('NEW URL', new_url)
+      return new_url
+    else:
+      return url
+
+    
+    
 
 def get_ending(linkname):
   if linkname:
@@ -26,9 +42,9 @@ def get_ending(linkname):
     if q_mark_found != -1:
       remove_querystring = ending.split("?")
       sanitized_ending = remove_querystring[0]
-      print("SANITIZED ENDING FOUND", sanitized_ending)
+      # print("SANITIZED ENDING FOUND", sanitized_ending)
       return sanitized_ending
-    print('ENDING FOUND', ending)
+    # print('ENDING FOUND', ending)
     return ending
     # get last el of the string
 
@@ -43,15 +59,18 @@ def check_if_file_is_audio_link(poss_link):
     # NOTE derived from https://en.wikipedia.org/wiki/Audio_file_format
     audio_formats = ["3gp", "aac", "act", "aiff", "alac", "amr", "flac", "m4a", "m4b", "mp3", "mp4", "mpc", "mogg", "oga", "ogg", "tta", "wav", "wv"]
     ending = get_ending(poss_link)
-
-    # TODO it would be nice to use regex to check for the items above? 
-
+    # TODO it would be nice to use regex to check for the items but this doesn't work? 
+    # poss_audio = re.search("\b(3gp|aac|act|aiff|alac|amr|flac|m4a|m4b|mp3|mp4|mpc|mogg|oga|ogg|tta|wav|wv)\w*\b", ending)
+    # print('AUDDDOOOO', poss_audio, ending)
+    
     for format in audio_formats:
       if ending == format:
         return ending
     
     return None
-      
+
+# TODO This would be good as a class and series of functions within the class
+# TODO This would be wonderful as an installable CLI app.
 
 def main():
   try:
@@ -73,6 +92,8 @@ def main():
     title = input('Enter the Podcast name: ')
     feed = input("Enter the feed xml url (e.g. http://podcastdomain.com/path/to/file.xml and not feed:http://): ")
 
+    removed_feed = remove_feed_from_url(feed)
+
     # TODO restore this Check if feed is vaild URL
     # parsedUrl = urlparse(feed)
 
@@ -86,23 +107,7 @@ def main():
     # ProtoNewsFeed = feedparser.parse('feed:' + feed)
     # NewsFeed = ProtoNewsFeed['feed']
 
-    # NOTE FEED should be http and not feed:http
-    parsedFromParser = podcastparser.parse(feed, urllib.request.urlopen(feed))
-  
-
-      # NewsFeed = feedparser.parse(feed)
-  
-    # print('done &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&', )
-
-    # Check to see Channel and Title exist
-    # print('  Confirming valid title ...', end = '')
-    # if title.__len__ == 0:
-    #   if type(NewsFeed) is not dict or not NewsFeed.has_key('channel'):
-    #     # if not, throw an exception
-    #     raise Exception("Invalid channel or title", NewsFeed)
-    #   else:
-    #     title = NewsFeed['channel']['title']
-    
+    parsedFromParser = podcastparser.parse(removed_feed, urllib.request.urlopen(removed_feed))
 
     # Get the Channel Title
     channelTitlePath = downloadsPath + "/" + title
@@ -125,12 +130,11 @@ def main():
         for ep in parsedFromParser["episodes"]:
           # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~EP~~~~~~~~~~~~~~~~~~~~~~",pp.pprint(ep.get("enclosures", 'No Enclosures Found')))
           possible_enclosures = ep.get("enclosures", None)
-          # TODO we should get the extension of the file of the url and use it rather than manually setting mp3
           
           if ep["title"]:
-            filename = ep["title"] #+ ".mp3"
+            filename = ep["title"]
           elif parsedFromParser["description"]:
-            filename = parsedFromParser["description"] + '-{}'.format(ep_count) #".mp3"
+            filename = parsedFromParser["description"] + '-{}'.format(ep_count)
           else:
             filename = " %d " % (ep_count)
 
@@ -147,10 +151,11 @@ def main():
                 last_enclosure = maybe_url
           
 
-          # TODO For next time, sanitize file names
+          # TODO For next time, sanitize file names removing /
           # TODO handle erroring out when link doesn't end in an audio file extension
 
 
+          # TODO Get the date and time of publication and if it exists, add it to the
           # titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
           # filename = channelTitlePath + "/" + str(titleDateObj.date()) + " - " + x.title + ".mp3"
           # print('  downloading filename: %s ... ' % filename, end = '')
@@ -158,21 +163,9 @@ def main():
           # open(filename, 'wb').write(r.content)
           ep_count = ep_count + 1
           print('done')
-          # TODO handle if parsedfromparser doesn't exist
     except Exception as error:
       print('Nested Exception parsedFromParser error', error, 'parsedFromParser', pprint.pformat(parsedFromParser, indent=2, sort_dicts=True))
 
-    # Download the MP3 files
-    # try:
-    #   for x in NewsFeed.entries:
-    #     titleDateObj = datetime.datetime.strptime(x.published, '%a, %d %b %Y %H:%M:%S %z')
-    #     filename = channelTitlePath + "/" + str(titleDateObj.date()) + " - " + x.title + ".mp3"
-    #     print('  downloading filename: %s ... ' % filename, end = '')
-    #     r = requests.get(x.links[0].href, allow_redirects=True)
-    #     open(filename, 'wb').write(r.content)
-    #     print('done')
-    # except Exception as error:
-    #   print('Nested Exception newsfeed error', error, 'NewsFeed', NewsFeed)
 
   except Exception as error:
     print("Error:", error, parsedFromParser, 'catch')
